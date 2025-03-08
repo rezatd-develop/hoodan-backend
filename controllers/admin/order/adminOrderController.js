b9c81ac5206e0072de4c3347b5e504a522a5b332
rezatd-develop <reza.td.develop@gmail.com>
2025-03-08 17:35:18 +0330

    complete admin orders

--- controllers/admin/order/adminOrderController.js
+++ controllers/admin/order/adminOrderController.js
@@ -76,7 +76,7 @@ exports.removeOrder = async (req, res) => {
         if (!deleted) {
             return res.status(404).json({ error: 'Order not found.' });
         }
-        res.status(200).json({ message: 'Order removed successfully' });
+        res.status(200).json({ message: 'Order removed successfully', hasError: false });
     } catch (err) {
         res.status(500).json({ error: 'Failed to remove order', message: err.message });
     }
