a1bff1de2990bad5176818e72ec13d7414d6a41a
rezatd-develop <reza.td.develop@gmail.com>
2025-04-20 17:56:24 +0330

    update cors

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const { startListeningServer } = require('./utilities/listener/listener')
 const path = require("path");
 const app = express();
 
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(cors({
     origin: true,
     credentials: true,
