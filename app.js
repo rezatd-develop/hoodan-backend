9e61880c27a71028f0832bcb2cb7db29b543dbbb
rezatd-develop <reza.td.develop@gmail.com>
2025-04-20 16:37:26 +0330

    test cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mainRouter = require('./routes/mainRouter');
 const accessControlHeadersApp = require('./utilities/accessControlHeaders/accessControlHeaders');
 const startupMiddlewaresApp = require('./utilities/startupMiddlewares/startupMiddlewares');
@@ -8,7 +9,7 @@ const { startListeningServer } = require('./utilities/listener/listener')
 
 const path = require("path");
 
-
+app.use(cors());
 const app = express();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(startupMiddlewaresApp);
