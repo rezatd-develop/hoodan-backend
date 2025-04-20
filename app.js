927c1c019013f1ce6bc48d666ca7ad5e5d49a500
rezatd-develop <reza.td.develop@gmail.com>
2025-04-20 17:54:26 +0330

    update cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const path = require("path");
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:80', 'http://156.253.5.235', 'http://156.253.5.235:3000', 'http://156.253.5.235:5000', 'http://156.253.5.235:3001'],
+    origin: '*',
     credentials: true,
 }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
