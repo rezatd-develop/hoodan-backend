a1edddb96a19545d6d039807fafecc747345c634
rezatd-develop <reza.td.develop@gmail.com>
2025-04-20 17:21:29 +0330

    cors solving

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mainRouter = require('./routes/mainRouter');
 const accessControlHeadersApp = require('./utilities/accessControlHeaders/accessControlHeaders');
@@ -7,9 +8,12 @@ const databaseConnectorApp = require('./utilities/database/databaseConnector');
 const { startListeningServer } = require('./utilities/listener/listener')
 
 const path = require("path");
-
-
 const app = express();
+
+app.use(cors({
+    origin: 'http://156.253.5.235',
+    credentials: true,
+}));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(startupMiddlewaresApp);
 app.use(accessControlHeadersApp);
