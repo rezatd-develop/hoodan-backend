17be639bc808191a4eb5b1e98b28bb90bf54d4cf
rezatd-develop <reza.td.develop@gmail.com>
2025-04-20 16:45:33 +0330

    solving cors error

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const databaseConnectorApp = require('./utilities/database/databaseConnector');
 const { startListeningServer } = require('./utilities/listener/listener')
 
 const path = require("path");
+const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000', 
     credentials: true
 }));
 
-const app = express();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(startupMiddlewaresApp);
 app.use(accessControlHeadersApp);
