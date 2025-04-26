059d8ccd1195f66887a4c8e47dc3620351457a4e
rezatd-develop <reza.td.develop@gmail.com>
2025-04-26 12:57:43 +0330

    update creds

--- utilities/database/databaseConnector.js
+++ utilities/database/databaseConnector.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = 'mongodb://rezatd:098123JUju.%40localhost:27017/hoodan?authSource=hoodan';
+    const mongoURI = 'MONGO_URI=mongodb://rezatd:098123JUju./hoodan?authSource=hoodan';
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
