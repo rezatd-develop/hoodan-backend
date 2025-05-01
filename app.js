15678387ed3c15d7148a0580c2bf6e6890eed316
rezatd-develop <reza.td.develop@gmail.com>
2025-05-01 18:30:02 +0400

    sms config

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-require('dotenv').config();
+// require('dotenv').config();
 const cors = require('cors');
 const helmet = require('helmet');
+const { farazSMS } = require('@aspianet/faraz-sms')
 const express = require('express');
 const mainRouter = require('./routes/mainRouter');
 const accessControlHeadersApp = require('./utilities/accessControlHeaders/accessControlHeaders');
 const startupMiddlewaresApp = require('./utilities/startupMiddlewares/startupMiddlewares');
 const databaseConnectorApp = require('./utilities/database/databaseConnector');
-const { startListeningServer } = require('./utilities/listener/listener')
+const { startListeningServer } = require('./utilities/listener/listener');
 
 const path = require("path");
 const app = express();
+farazSMS.init('OWVjMjZiMmQtOWM5Yy00NjU0LWFiODItZjE5YjZjZTFhYTFlNTc1OGQwMmVhZWJlYjIyMGJkMTU5NDM2NDlkNDVkZmY=');
 
 app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
--- utilities/otp/otp.js
+++ utilities/otp/otp.js
@@ -1,9 +1,25 @@
 const otpStore = new Map();
+const { farazSendPattern } = require('@aspianet/faraz-sms');
+const axios = require('axios')
 
 exports.sendOtp = async (phone) => {
   const code = Math.floor(100000 + Math.random() * 900000).toString();
+
+  axios.post(
+    'https://api2.ippanel.com/api/v1/sms/pattern/normal/send',
+    {
+      "code": "jft6hxvbzy722jp",
+      "sender": "+983000505",
+      "recipient": "+989390753192",
+      "variable": {
+        "verification-code": code
+      }
+    },
+    { headers: { Authorization: 'OWVjMjZiMmQtOWM5Yy00NjU0LWFiODItZjE5YjZjZTFhYTFlNTc1OGQwMmVhZWJlYjIyMGJkMTU5NDM2NDlkNDVkZmY=' } }
+  );
+
+
   otpStore.set(phone, code);
-  console.log(`OTP for ${phone}: ${code}`);
 };
 
 exports.verifyOtp = async (phone, code) => {
