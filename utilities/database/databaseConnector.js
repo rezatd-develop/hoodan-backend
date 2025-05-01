ae44b45d59b3e9d53b768687ad75879dcd73e9df
jack-philips <reza.td.develop@gmail.com>
2025-05-01 19:58:28 +0330

    update mongo

--- utilities/database/databaseConnector.js
+++ utilities/database/databaseConnector.js
@@ -1,27 +1,22 @@
 const mongoose = require('mongoose');
 
+// Suppress warning and explicitly set behavior:
+mongoose.set('strictQuery', true); 
+// or:
+// mongoose.set('strictQuery', false);
+
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://156.253.5.235:27017/hoodan', {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
-
-    mongoose.connection.once('open', () => {
-      console.log('MongoDB connected successfully');
-    });
-
-    mongoose.connection.on('error', (err) => {
-      console.error('MongoDB connection error:', err);
-      process.exit(1);
-    });
-
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.error('Initial MongoDB connection error:', error);
+    console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
 
 connectDB();
-
 module.exports = connectDB;
