f7d6536f1b7e6991c9a351324f941c7233d1cb75
rezatd-develop <reza.td.develop@gmail.com>
2026-07-25 15:58:04 +0330

    improve environment values

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-// require('dotenv').config();
+require('dotenv').config();
+
 const cors = require('cors');
 const helmet = require('helmet');
-const { farazSMS } = require('@aspianet/faraz-sms')
+const { farazSMS } = require('@aspianet/faraz-sms');
 const express = require('express');
+const path = require('path');
+
 const mainRouter = require('./routes/mainRouter');
 const accessControlHeadersApp = require('./utilities/accessControlHeaders/accessControlHeaders');
 const startupMiddlewaresApp = require('./utilities/startupMiddlewares/startupMiddlewares');
 const databaseConnectorApp = require('./utilities/database/databaseConnector');
 const { startListeningServer } = require('./utilities/listener/listener');
 
-const path = require("path");
 const app = express();
-farazSMS.init('OWVjMjZiMmQtOWM5Yy00NjU0LWFiODItZjE5YjZjZTFhYTFlNTc1OGQwMmVhZWJlYjIyMGJkMTU5NDM2NDlkNDVkZmY=');
 
-app.use(cors());
+farazSMS.init(process.env.FARAZ_SMS_API_KEY);
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true,
+}));
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(startupMiddlewaresApp);
 app.use(accessControlHeadersApp);
 app.use(mainRouter);
 app.use(databaseConnectorApp);
+
 startListeningServer(app);
 
 module.exports = app;
--- utilities/database/databaseConnector.js
+++ utilities/database/databaseConnector.js
@@ -1,16 +1,13 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
-// Suppress warning and explicitly set behavior:
-mongoose.set('strictQuery', true); 
-// or:
-// mongoose.set('strictQuery', false);
+// Suppress warning
+mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://rezatd:MohsenLorestan7234788@156.253.5.235:27017/admin?authSource=admin', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
@@ -18,5 +15,4 @@ const connectDB = async () => {
   }
 };
 
-connectDB();
 module.exports = connectDB;
