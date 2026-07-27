78ea8838d6ea558445f503c2e44ab076ab1214fe
rezatd-develop <reza.td.develop@gmail.com>
2026-07-27 16:13:57 +0330

    change database configs

--- utilities/database/databaseConnector.js
+++ utilities/database/databaseConnector.js
@@ -9,7 +9,10 @@ console.log('***process.env.MONGO_URI', process.env.MONGO_URI)
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
